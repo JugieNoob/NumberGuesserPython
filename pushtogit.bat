30d69aec54f94f63595115906eb15d708d2d5dcb
JugieNoob <jurgenarna@gmail.com>
2023-04-03 12:54:37 +0200

    Added TicTacToe Game

--- pushtogit.bat
+++ pushtogit.bat
@@ -4,7 +4,7 @@ git add .
 git status
 set /p NameOfCommit=What what you like to name your commit?
 git commit -m "%NameOfCommit%"
-git pull
+git pull --rebase origin main
 git push -u origin main
 echo "Press ENTER to close this window!"
 set /p EnterToClose=
