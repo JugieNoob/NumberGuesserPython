352033ac3cb139ba6e38f14aa8984516179ca1e8
JugieNoob <jurgenarna@gmail.com>
2023-04-03 12:51:34 +0200

    Added Tic Tac Toe Game

--- pushtogit.bat
+++ pushtogit.bat
@@ -4,7 +4,6 @@ git add .
 git status
 set /p NameOfCommit=What what you like to name your commit?
 git commit -m "%NameOfCommit%"
-git remote add origin https://github.com/JugieNoob/NumberGuesserPython.git
 git push -u origin master
 echo "Press ENTER to close this window!"
 set /p EnterToClose=
